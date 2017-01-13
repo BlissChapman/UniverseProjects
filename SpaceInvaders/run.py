1c6633b5efa5f1a2f7c1c39507734ef6f99b18ed
BlissChapman <bliss@agrible.com>
2017-01-12 20:03:53 -0600

    Initial commit of SpaceInvaders files.

---
+++ SpaceInvaders/run.py
@@ -0,0 +1,24 @@
+import gym
+import universe  # register the universe environments
+
+from agent import RandomAgent
+
+env = gym.make("CartPole-v0")
+
+env.seed(0)
+agent = RandomAgent(env.action_space)
+
+episode_count = 100
+reward = 0
+done = False
+
+for i in range(episode_count):
+    ob = env.reset()
+    while True:
+        action = agent.act(ob, reward, done)
+        ob, reward, done, _ = env.step(action)
+
+        env.render()
+        if done:
+            print("Episode Complete")
+            break
